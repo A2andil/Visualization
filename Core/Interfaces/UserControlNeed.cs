f76002717c4059769261d3e12d7bb3ba36680b04
A2andil <A77mad.22andil@gmail.com>
2020-09-23 17:00:38 +0200

    UserControlNeed

---
+++ Core/Interfaces/UserControlNeed.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Core.Interfaces
+{
+    public interface UserControlNeed
+    {
+        public void Random();
+
+        public void DrawElements();
+
+        public void CreateElements();
+    }
+}
